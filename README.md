e771daeb9b99bc444c3e2a56f566f1baad750ff1
JerryJohn <jzynoter@gmail.com>
2019-04-20 10:18:28 -0700

    添加数据预处理

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - python 3.6
     - sklearn: 教程网址 https://scikit-learn.org/stable/
     - pytorch 1.0
+
+#### 数据预处理
+  
+将下载自百度的数据文件解压后放在与该文件夹下，运行 `preprocess.py` 即可。
+    
+    python preprocess.py
---
+++ preprocess.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+
+import csv
+import numpy as np
+import pickle
+import json
+
+
+def get_data():
+    train_clicks_file = './data_set_phase1/train_clicks.csv'
+    train_queries_file = './data_set_phase1/train_queries.csv'
+    train_plans_file = './data_set_phase1/train_plans.csv'
+
+    user_click = []
+    clicks_reader = csv.reader(open(train_clicks_file, 'r'))
+    for line in clicks_reader:
+        if clicks_reader.line_num == 1:
+            continue
+        line[0] = int(line[0])
+        line[1] = int(line[2])
+        user_click.append(line)
+
+    pickle.dump(user_click, open('./data_set_phase1/train_clicks.pickle', 'wb'))
+
+    user_plans = []
+    clicks_reader = csv.reader(open(train_plans_file, 'r'))
+    for line in clicks_reader:
+        if clicks_reader.line_num == 1:
+            continue
+        line[0] = int(line[0])
+        line[2] = json.loads(line[2])
+        user_plans.append(line)
+
+    pickle.dump(user_plans, open('./data_set_phase1/train_plans.pickle', 'wb'))
+
+    user_queries = []
+    clicks_reader = csv.reader(open(train_queries_file, 'r'))
+    for line in clicks_reader:
+        if clicks_reader.line_num == 1:
+            continue
+        line[0] = int(line[0])
+        line[3] = list(map(float, line[3].split(',')))
+        line[4] = list(map(float, line[4].split(',')))
+        user_queries.append(line)
+
+    pickle.dump(user_queries, open('./data_set_phase1/train_queries.pickle', 'wb'))
+
+
+if __name__ == '__main__':
+    get_data()
